cd8c5f43bb3139de14a71747114a3c8f6b49a6ed
Jemila <jemila.abulhawa@gmail.com>
2019-09-24 23:47:06 +0300

    edit style modifiers and add hover color to link

--- src/components/elements/typography.js
+++ src/components/elements/typography.js
@@ -3,7 +3,7 @@ import { applyStyleModifiers } from 'styled-components-modifiers'
 import { typography, color, letter_spacing_negative as ls } from '../../utils'
 
 const { size } = typography
-const { white, secondary } = color
+const { white, accent, secondary } = color
 
 const COLOR_MODIFIERS = {
   white: () => `
@@ -30,8 +30,11 @@ const COLOR_MODIFIERS = {
 }
 
 const SIZE_MODIFIERS = {
-  large: () => `
+  size_small_medium: () => `
     font-size: ${size.s_md};
+`,
+  size_medium: () => `
+    font-size: ${size.md};
   `
 }
 
@@ -44,28 +47,28 @@ export const H1 = styled.h1`
   left: -25px;
   top: 100px;
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
 `
 
 export const H2 = styled.h2`
   font-size: ${size.md_lg};
   letter-spacing: ${ls.lg};
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
 `
 
 export const H3 = styled.h3`
   font-size: ${size.md};
   letter-spacing: ${ls.xs};
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
 `
 
 export const H4 = styled.h4`
   font-size: ${size.s};
   letter-spacing: ${ls.xs};
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
 `
 
 export const Link = styled.a`
@@ -73,13 +76,19 @@ export const Link = styled.a`
   font-weight: bold;
   letter-spacing: ${ls.xxs};
   text-decoration: underline;
+  transition: all 0.5s;
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
+  &:hover {
+    color: ${accent};
+  }
+
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
+  ${applyStyleModifiers(SIZE_MODIFIERS)};
 `
 
 export const P = styled.p`
-  font-size: ${size.xs};
+  font-size: ${size.s};
 
-  ${applyStyleModifiers(COLOR_MODIFIERS)}
-  ${applyStyleModifiers(SIZE_MODIFIERS)}
+  ${applyStyleModifiers(COLOR_MODIFIERS)};
+  ${applyStyleModifiers(SIZE_MODIFIERS)};
 `
